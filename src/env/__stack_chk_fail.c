9f57f3b3d5031c0b84420ff08fc46897b034fefa
joseph <joseph@jc-lab.net>
2021-02-03 12:04:25 +0900

    fix: missed MUSL_EXPORT

--- src/env/__stack_chk_fail.c
+++ src/env/__stack_chk_fail.c
@@ -2,9 +2,9 @@
 #include <stdint.h>
 #include "pthread_impl.h"
 
-uintptr_t __stack_chk_guard;
+MUSL_EXPORT uintptr_t __stack_chk_guard;
 
-void __init_ssp(void *entropy)
+MUSL_EXPORT void __init_ssp(void *entropy)
 {
 	if (entropy) memcpy(&__stack_chk_guard, entropy, sizeof(uintptr_t));
 	else __stack_chk_guard = (uintptr_t)&__stack_chk_guard * 1103515245;
@@ -12,7 +12,7 @@ void __init_ssp(void *entropy)
 	__pthread_self()->canary = __stack_chk_guard;
 }
 
-void __stack_chk_fail(void)
+MUSL_EXPORT void __stack_chk_fail(void)
 {
 	a_crash();
 }
